7fcaf6b64fc49e779131f39979c3bd2cadd81940
docmcstuffins33 <emdoubleu.yu@gmail.com>
2023-04-25 14:22:16 -0700

    added image

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req,res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
+app.get('/members', (req,res) => {
+    res.send("<img src='/most_wanted.png' style='width:250px;'>");
+});
+
+app.use(express.static(__dirname + "/public"));
+
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
 }); 
