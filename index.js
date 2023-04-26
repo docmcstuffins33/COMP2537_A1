27b23ac7e028961e8f246a15aa0b2f8a982a766e
Mylo <emdoubleu.yu@gmail.com>
2023-04-26 11:18:12 -0700

    added redirect button

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("<h1>Hello World!</h1>");
+    var html = `<h1>Welcome to the Archive</h1>
+        <form action='/members' method='redirect'>
+            <button>Members</button>
+        </form>
+        `
+    res.send(html)
 });
 
 app.get('/members', (req,res) => {
@@ -13,6 +18,11 @@ app.get('/members', (req,res) => {
 });
 
 app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req, res) => {
+    res.status(404);
+    res.send("Page not found - 404");
+})
 
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
