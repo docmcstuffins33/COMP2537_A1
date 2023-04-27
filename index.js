cbdfb078cf55280b9da398d09fa4826958ba49a9
Mylo <emdoubleu.yu@gmail.com>
2023-04-27 16:51:55 -0700

    added saving user data into database

--- index.js
+++ index.js
@@ -1,20 +1,180 @@
+require("./utils.js");
+require('dotenv').config();
+
 const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+const Joi = require("joi");
+
+const expireTime = 60 * 60 * 1000; //expires after 1 hour  (minutes * seconds * millis)
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+
+app.use(express.urlencoded({extended: false}));
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+    crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
 
 app.get('/', (req,res) => {
+    if(!req.session.authenticated) {
     var html = `<h1>Welcome to the Archive</h1>
-        <form action='/members' method='redirect'>
-            <button>Members</button>
+        <form action='/signup' method='redirect'>
+            <button>Sign up</button>
+        </form>
+        <form action='/login' method='redirect'>
+            <button>Log in</button>
         </form>
         `
-    res.send(html)
+    } else {
+        var html = `
+        <h1>Welcome to the Archive, ` + req.session.username+`</h1>
+        <form action='/members' method='redirect'>
+            <button>Go to members area</button>
+        </form>
+        <form action='/logout' method='redirect'>
+            <button>Log out</button>
+        </form>
+    `;
+    }
+    res.send(html);
+});
+
+app.get('/signup', (req, res) => {
+    var html = `
+    <h1>Sign Up</h1>
+    <form action='/submituser' method='post'>
+    <input name='username' type='text' placeholder='username'>
+    <input name='email' type='email' placeholder='email'>
+    <input name='password' type='password' placeholder='password'>
+    <button>Submit</button>
+    </form>
+    `
+    res.send(html);
+});
+
+app.post('/submituser', async (req,res) => {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    const schema = Joi.object(
+		{
+			username: Joi.string().alphanum().max(20).required(),
+            email: Joi.string().max(20).required(),
+			password: Joi.string().max(20).required()
+		});
+
+	const validationResult = schema.validate({username, email, password});
+	if (validationResult.error != null) {
+	   console.log(validationResult.error);
+	   res.redirect("/signup");
+	   return;
+   }
+
+    var hashedPassword = await bcrypt.hash(password, saltRounds);
+
+	await userCollection.insertOne({username: username, email: email, password: hashedPassword});
+	console.log("Inserted user");
+    req.session.authenticated = true;
+    req.session.userame = username;
+	req.session.email = email;
+	req.session.cookie.maxAge = expireTime;
+    res.redirect('/members');
+});
+
+app.get('/login', (req, res) => {
+    var html = `
+    <h1>Log In</h1>
+    <form action='/loggingin' method='post'>
+    <input name='email' type='email' placeholder='email'>
+    <input name='password' type='password' placeholder='password'>
+    <button>Submit</button>
+    </form>
+    `
+    res.send(html);
+});
+
+app.post('/loggingin', async (req,res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    const schema = Joi.string().max(20).required();
+	const validationResult = schema.validate(email);
+	if (validationResult.error != null) {
+	   console.log(validationResult.error);
+	   res.redirect("/login");
+	   return;
+	}
+
+    const result = await userCollection.find({email: email}).project({email: 1, username: 1, password: 1, _id: 1}).toArray();
+    console.log(result);
+	if (result.length != 1) {
+		console.log("user not found");
+		res.redirect("/login");
+		return;
+	}
+	if (await bcrypt.compare(password, result[0].password)) {
+		console.log("correct password");
+		req.session.authenticated = true;
+        req.session.username = result[0].username;
+		req.session.email = email;
+		req.session.cookie.maxAge = expireTime;
+
+		res.redirect('/members');
+		return;
+	}
+	else {
+		console.log("incorrect password");
+		res.redirect("/login");
+		return;
+	}
+});
+
+app.get('/logout', (req,res) => {
+	req.session.destroy();
+    res.redirect('/');
 });
 
 app.get('/members', (req,res) => {
-    res.send("<img src='/most_wanted.png' style='width:250px;'>");
+    if (!req.session.authenticated) {
+        res.redirect('/');
+    }
+    var html = `
+    <h1>Hello, ` + req.session.username +
+   `!</h1><img src='/most_wanted.png' style='width:250px;'>
+    `;
+    res.send(html);
 });
 
 app.use(express.static(__dirname + "/public"));
